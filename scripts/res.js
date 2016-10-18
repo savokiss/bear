b1083f710aa5c5fdcc33d57fe42661434abad86a
savokiss <jaynaruto@qq.com>
2016-10-18 11:27:54 +0800

    use body

--- scripts/res.js
+++ scripts/res.js
@@ -4,14 +4,14 @@ module.exports = function(robot) {
   robot.hear(/res$/, function(msg) {
     return request('http://www.radida.com/pacs/res.json', function(err, res, body) {
       console.log(body);
-      msg.reply(JSON.parse(body));
+      msg.reply(body);
     })
   })
 
   robot.hear(/res_cloud$/, function(msg) {
     return request('http://www.radida.com/pacs_cloud/res.json', function(err, res, body) {
       console.log(body)
-      msg.reply(JSON.parse(body));
+      msg.reply(body);
     })
   })
 }
