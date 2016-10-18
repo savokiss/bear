c8c6f98773b08bcc1b9556ba8d71e4e17d000a45
savokiss <jaynaruto@qq.com>
2016-10-18 12:16:51 +0800

    no template string

--- scripts/res.js
+++ scripts/res.js
@@ -4,14 +4,14 @@ module.exports = function(robot) {
   robot.hear(/res$/, function(msg) {
     return request('http://www.radida.com/pacs/res.json', function(err, res, body) {
       var version = JSON.parse(body).version;
-      msg.reply(`res version is $version`);
+      msg.reply('res version is ' + version);
     })
   })
 
   robot.hear(/res_cloud$/, function(msg) {
     return request('http://www.radida.com/pacs_cloud/res.json', function(err, res, body) {
       var version = JSON.parse(body).version;
-      msg.reply(`res_cloud version is $version`);
+      msg.reply('res_cloud version is ' + version);
     })
   })
 }
