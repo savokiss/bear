47905134176d1591c086533660870bf9bda24a95
savokiss <jaynaruto@qq.com>
2016-10-18 10:54:27 +0800

    use http

--- scripts/res.js
+++ scripts/res.js
@@ -1,10 +1,15 @@
-  var http = require('http')
+var http = require('http')
 
-  http.get({
-    hostname: 'www.radida.com',
-    port: 80,
-    path: '/pacs/res.json',
-    agent: false // create a new agent just for this one request
-  }, function(res) {
-    console.log(res);
-  });
+module.exports = function(robot) {
+  robot.respond(/res$/, function(msg) {
+    http.get({
+      hostname: 'www.radida.com',
+      port: 80,
+      path: '/pacs/res.json',
+      agent: false // create a new agent just for this one request
+    }, function(res) {
+      console.log(res);
+      console.log(msg);
+    });
+  })
+}
