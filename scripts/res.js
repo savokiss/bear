f3e8069ea9ec269ec76a3314b73ac34496671319
savokiss <jaynaruto@qq.com>
2016-10-18 12:15:28 +0800

    version

--- scripts/res.js
+++ scripts/res.js
@@ -3,15 +3,15 @@ var request = require('request')
 module.exports = function(robot) {
   robot.hear(/res$/, function(msg) {
     return request('http://www.radida.com/pacs/res.json', function(err, res, body) {
-      console.log(body);
-      msg.reply(body);
+      var version = JSON.parse(body).version;
+      msg.reply(`res version is $version`);
     })
   })
 
   robot.hear(/res_cloud$/, function(msg) {
     return request('http://www.radida.com/pacs_cloud/res.json', function(err, res, body) {
-      console.log(body)
-      msg.reply(body);
+      var version = JSON.parse(body).version;
+      msg.reply(`res_cloud version is $version`);
     })
   })
 }
