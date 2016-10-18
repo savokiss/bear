1ad16943d2ec6ecd84c5773f93b2479ce39ed03b
savokiss <jaynaruto@qq.com>
2016-10-18 11:18:39 +0800

    return request

--- scripts/res.js
+++ scripts/res.js
@@ -2,14 +2,14 @@ var request = require('request')
 
 module.exports = function(robot) {
   robot.respond(/res$/, function(msg) {
-    request('http://www.radida.com/pacs/res.json', function(err, res, body) {
+    return request('http://www.radida.com/pacs/res.json', function(err, res, body) {
       console.log(body);
       msg.send(JSON.parse(body));
     })
   })
 
   robot.respond(/res_cloud$/, function(msg) {
-    request('http://www.radida.com/pacs_cloud/res.json', function(err, res, body) {
+    return request('http://www.radida.com/pacs_cloud/res.json', function(err, res, body) {
       console.log(body)
       msg.send(JSON.parse(body));
     })
