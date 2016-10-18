df769eb4d39b88f533653ee4d568bfd463c5ee97
savokiss <jaynaruto@qq.com>
2016-10-18 11:26:17 +0800

    use hear

--- scripts/res.js
+++ scripts/res.js
@@ -1,17 +1,17 @@
 var request = require('request')
 
 module.exports = function(robot) {
-  robot.respond(/res$/, function(msg) {
+  robot.hear(/res$/, function(msg) {
     return request('http://www.radida.com/pacs/res.json', function(err, res, body) {
       console.log(body);
-      msg.send(JSON.parse(body));
+      msg.reply(JSON.parse(body));
     })
   })
 
-  robot.respond(/res_cloud$/, function(msg) {
+  robot.hear(/res_cloud$/, function(msg) {
     return request('http://www.radida.com/pacs_cloud/res.json', function(err, res, body) {
       console.log(body)
-      msg.send(JSON.parse(body));
+      msg.reply(JSON.parse(body));
     })
   })
 }
